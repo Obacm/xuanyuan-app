294a7db2e6cb6504de80a4c647830fe93d160b1f
Obacm <1129627438@qq.com>
2019-09-21 16:08:54 +0800

    bug

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <transition name="custom-animated rotateOut" enter-active-class="animated rotateOut">
-      <router-view />
-    </transition>
+    <router-view />
   </div>
 </template>
 
