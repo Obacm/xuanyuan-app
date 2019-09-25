657861124d70626a3b35b3e5216a96001d722cb1
Obacm <1129627438@qq.com>
2019-09-25 15:36:58 +0800

    经验

---
+++ src/views/Grade.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-container fluid>
+      
+  </b-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  computed: {
+    ...mapGetters([
+    ]),
+  },
+  watch: {
+    
+  }
+};
+</script>
+
+<style>
+
+</style>
