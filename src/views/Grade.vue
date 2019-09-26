d98cf4eb8f82169bf9800780bb1b91a3add76491
Obacm <1129627438@qq.com>
2019-09-26 09:37:39 +0800

    优化

--- src/views/Grade.vue
+++ src/views/Grade.vue
@@ -1,24 +1,30 @@
 <template>
   <b-container fluid>
     <div class="box">
-      <b-badge href="#" variant="dark">当前境界</b-badge>
+      <b-badge variant="dark">当前境界</b-badge>
     </div>
     <div class="box">
       <span class="badge badge-pill" :class="currentGrade === grade.value ? 'badge-primary': 'badge-light'" v-for="(grade, index) in grades" :key="index" @click="setGrade(grade.value)">{{ grade.text }}</span>
     </div>
     <div class="box">
-      <b-badge href="#" variant="dark">当前等级</b-badge>
+      <b-badge variant="dark">当前等级</b-badge>
     </div>
     <div class="box">
       <span class="badge" :class="currentNode === node ? 'badge-primary': 'badge-light'" v-for="(node, index) in nodes" :key="index" @click="setNode(node)">{{ node }}</span>
     </div>
     <div class="box">
-      <b-badge href="#" variant="dark">当前悟性</b-badge>
+      <b-badge variant="dark">当前悟性</b-badge>
     </div>
     <div class="box">
       <div class="m-4">
-        <b-form-input v-model="currentPower" type="range" min=0 max=200000></b-form-input>
-        <div class="mt-2">{{ currentPower }}</div>
+        <b-form-input v-model="currentPowerWan" type="range" min=0 max=100000 step=10000></b-form-input>
+        <b-form-input v-model="currentPowerQian" type="range" min=0 max=10000 step=1000></b-form-input>
+        <b-form-input v-model="currentPowerBai" type="range" min=0 max=1000 step=100></b-form-input>
+        <b-form-input v-model="currentPowerShi" type="range" min=0 max=100 step=10></b-form-input>
+        <b-form-input v-model="currentPowerGe" type="range" min=0 max=10 step=1></b-form-input>
+        <div class="mt-2">
+          <b-badge href="#" variant="light">{{ currentPower }}</b-badge>
+        </div>
       </div>
     </div>
     <div class="box">
@@ -45,7 +51,11 @@ export default {
       type: 1,
       currentGrade: 1,
       currentNode: 1,
-      currentPower: 0,
+      currentPowerWan: 0,
+      currentPowerQian: 0,
+      currentPowerBai: 0,
+      currentPowerShi: 0,
+      currentPowerGe: 0,
       options: [
         { text: '境界', value: 1 },
         { text: '肉身', value: 2 },
@@ -83,6 +93,9 @@ export default {
     powerOver() {
       return parseInt(this.currentPower) > 0;
     },
+    currentPower() {
+      return parseInt(this.currentPowerWan) + parseInt(this.currentPowerQian) + parseInt(this.currentPowerBai) + parseInt(this.currentPowerShi) + parseInt(this.currentPowerGe);
+    }
   },
   watch: {
     
